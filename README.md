fb654b323c82b0e48e5cc48c00e9a3acc6bd26eb
xudafeng <xudafeng@126.com>
2017-04-11 23:31:24 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# cv-sample-python
+
+## Run
+
+``` bash
+$ pip install -r requirements.txt
+$ python macaca-desktop-sample.test.py
+```
+
+## License
+
+The MIT License (MIT)
---
+++ opencv-sample.py
@@ -0,0 +1,25 @@
+#coding:utf-8
+
+import cv2
+import numpy as np
+
+class opencvSample:
+
+    def __init__(self, sourceimage):
+        self.sourceimage = sourceimage
+
+    def match(self,templateimage, threshold=0.8):
+        image = cv2.imread(self.sourceimage)
+        template = cv2.imread(templateimage)
+        result = cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
+        similarity = cv2.minMaxLoc(result)[1]
+        if similarity < threshold:
+            return similarity
+        else:
+            return np.unravel_index(result.argmax(), result.shape)
+
+if __name__ == '__main__':
+    matcher = opencvSample('./fixture/T-Shirt.jpg')
+    for item in ['./fixture/T-Shirt-logo.jpg', './fixture/logo.png']:
+        print item, matcher.match(item)
+
